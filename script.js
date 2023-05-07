2bbfbd63c66b5a4b464e5387090458fb2789e032
givanauskaite <gereta7@gmail.com>
2023-05-07 23:48:29 +0300

    burgeris ir stiliaus bsk

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ var position = 0;
             navbar.style.background = "transparent"
         }
     }
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
