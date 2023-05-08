8bb39256a0b7c031a89976d1718bcaf5664384fd
givanauskaite <gereta7@gmail.com>
2023-05-08 13:57:05 +0300

    tvarkymasis

--- script.js
+++ script.js
@@ -26,3 +26,20 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+
+
+function openForm(evt, formName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    document.getElementById(formName).style.display = "block";
+    evt.currentTarget.className += "active";
+
+}
